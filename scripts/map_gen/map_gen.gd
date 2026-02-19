92d185081c393002dae5df05c662a0623bb6f0e1
Lorren Gordon <lorrenrules@gmail.com>
2026-02-19 15:49:11 -0600

    Bonun, Turnstyle sprites, actor updates, atmosphere

--- scripts/map_gen/map_gen.gd
+++ scripts/map_gen/map_gen.gd
@@ -3,32 +3,32 @@ extends Node2D
 @onready var texture = NoiseTexture2D.new()
 @export var textureRect = TextureRect
 
-@onready var xOff = 0
-@onready var yOff = 0
+var xOff = 0
+var yOff = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	texture.set_width(2000)
-	texture.set_height(2000)
+	texture.set_width(1000)
+	texture.set_height(500)
 	texture.noise = FastNoiseLite.new()
+	texture.noise.frequency = 0.008
 	texture.noise.set_noise_type(3)
 	texture.noise.seed = randi()
-	#texture.noise.set_domain_warp_enabled(true)
-	#texture.noise.set_domain_warp_amplitude(200.0)
+	texture.color_ramp = Gradient.new()
+	texture.color_ramp.set_offset(0, 0.4)
+	texture.color_ramp.set_offset(1, 0.8)
+	texture.color_ramp.set_color(0, Color(0.686, 0.0, 0.0, 0.2))
+	texture.color_ramp.set_color(1, Color(0.789, 0.567, 0.578, 0.3))
 	await texture.changed
 	var image = texture.get_image()
 	var data = image.get_data()
+	scale = Vector2(5.0, 5.0)
 	textureRect.texture = texture
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	xOff += delta * 80.0
-	yOff += delta * 80.0
+	xOff += delta * 15.0
 	texture.noise.offset = Vector3(xOff, yOff, 0.0)
 	pass
-	
-#func _input(inp: InputEvent) -> void:
-	#if inp.
-	#pass
