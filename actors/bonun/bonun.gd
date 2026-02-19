bafbd9a7b118d9b95359f725af38e52f4899477f
Andrew Farinella <andrew.farinella@gmail.com>
2026-02-18 22:02:58 -0600

    Implement bone fragment projectile, don’t have gunners fire if they are off screen

--- actors/bonun/bonun.gd
+++ actors/bonun/bonun.gd
@@ -4,6 +4,7 @@ extends Node2D
 @onready var weapon:Weapon = $Weapon as Weapon
 @onready var moveComponent: MoveComponent = $MoveComponent
 @onready var dropsComponent: DropsComponent = $DropsComponent
+@onready var visibleNotifier: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,16 +14,13 @@ func _ready() -> void:
 func fire_weapon() -> void:
 	await get_tree().create_timer(weapon.fireRateInSeconds).timeout
 	if GameManager.hostCharacter == null: return
+	if !visibleNotifier.is_on_screen():
+		call_deferred("fire_weapon")
+		return
 	var startingRotation := rotation_degrees
 	look_at(GameManager.hostCharacter.global_position)
 	rotate(deg_to_rad(90))
 	var endingRotation := rotation_degrees
 	moveComponent.velocity = moveComponent.velocity.rotated(deg_to_rad(endingRotation - startingRotation))
-	
 	weapon.fire_at_angle(rotation_degrees)
-	#var bullet = spawnerComponent.spawn(global_position, %Projectiles) as Bullet
-	#bullet.look_at(GameManager.hostCharacter.global_position)
-	#bullet.rotate(deg_to_rad(90))
-	#bullet.target_player()
-	fire_weapon()
-	pass
+	call_deferred("fire_weapon")
