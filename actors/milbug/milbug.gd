b143aa6c879934be07aa11bfe55223e46a8c04b8
Lorren Gordon <lorrenrules@gmail.com>
2026-02-20 13:26:45 -0600

    milbug and death trap sprites and adjustments

--- actors/milbug/milbug.gd
+++ actors/milbug/milbug.gd
@@ -1,3 +1,7 @@
 class_name Milbug
 extends Node2D
 @onready var dropsComponent: DropsComponent = $DropsComponent
+
+func _physics_process(delta: float) -> void:
+	rotation = lerp_angle(rotation, rotation + get_angle_to(GameManager.hostCharacter.global_position), delta * 1.5)
+	
